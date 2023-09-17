262010856884a380b2e18cd5efb558d771c62f74
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-09-17 20:09:26 +0300

    header add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import SharedLayout from '../components/SharedLayout/SharedLayout';
+import SharedLayout from '../components/SharedLayout';
 
 const Home = lazy(() => import('../pages/Home'));
 const Catalog = lazy(() => import('../pages/Catalog'));
@@ -10,11 +10,12 @@ const Favorites = lazy(() => import('../pages/Favorites'));
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<SharedLayout />}></Route>
-      <Route index element={<Home />} />
-      <Route path="/catalog" element={<Catalog />} />
-      <Route path="/favorites" element={<Favorites />} />
-      <Route path="*" element={<Home />} />
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="*" element={<Home />} />
+      </Route>
     </Routes>
   );
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,18 +1,38 @@
-import { NavLink } from 'react-router-dom';
-import css from "./Nav.module.css"
+import { NavLink, useLocation } from 'react-router-dom';
+import css from './Nav.module.css';
 
 export default function Nav() {
+  const location = useLocation();
   return (
-    <nav className={css.navList}>
-      <NavLink className={css.navLink} to="/">
+    <nav className={css.nav}>
+      <NavLink
+        className={
+          location.pathname === '/'
+            ? [css.find, css.active].join(' ')
+            : css.find
+        }
+        to="/"
+      >
         Home
       </NavLink>
-
-      <NavLink className={css.navLink} to="/catalog">
+      <NavLink
+        className={
+          location.pathname === '/catalog'
+            ? [css.find, css.active].join(' ')
+            : css.find
+        }
+        to="/catalog"
+      >
         Catalog
       </NavLink>
-
-      <NavLink className={css.navLink} to="/favorites">
+      <NavLink
+        className={
+          location.pathname === '/favorites'
+            ? [css.find, css.active].join(' ')
+            : css.find
+        }
+        to="/favorites"
+      >
         Favorites
       </NavLink>
     </nav>
