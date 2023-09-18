4c9f08c724c12eba9ea7d5777c50c7f6d6800081
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-09-18 16:50:11 +0300

    fix basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import 'modern-normalize/modern-normalize.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/car-finder">
+    <BrowserRouter basename="car-finder">
       <App />
     </BrowserRouter>
   </React.StrictMode>
