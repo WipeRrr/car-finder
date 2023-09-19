c96dec803674b09e1c54ac071ee5355bae219681
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-09-19 22:09:05 +0300

    add btn favorite

--- src/components/CarsList/CarsList.jsx
+++ src/components/CarsList/CarsList.jsx
@@ -5,6 +5,7 @@ import getCountry from 'utils/getCountry';
 import Modal from 'components/Modal';
 import CarModal from 'components/CarModal/CarModal';
 import Button from 'components/Button';
+import heart from '../../images/sprite.svg';
 export default function CarsList({ car }) {
   const [showModal, setShowModal] = useState(false);
   const toggleModal = () => setShowModal(!showModal);
@@ -32,41 +33,45 @@ export default function CarsList({ car }) {
   const city = getCity(address);
 
   return (
-      <li className={css.car_item}>
-        <div className={css.card}>
-          <div>
-            <img className={css.car_img} src={img} alt="car" />
-          </div>
-
-          <div className={css.info}>
-            <p>
-              {make}
-              {make.length <= 5 && (
-                <span className={css.model}> {model}</span>
-              )}, {year}
-            </p>
-            <p>{rentalPrice}</p>
-          </div>
-          <div className={css.about}>
-            <p>
-              <span>{city} | </span>
-              <span>{country} | </span>
-              <span>{rentalCompany} | </span>
-            </p>
-
-            <p>
-              <span>{type} | </span>
-              <span>{model} | </span>
-              <span>{id} | </span>
-            </p>
-          </div>
-          <Button text={'Learn more'} onClick={toggleModal} />
-          {showModal && (
-            <Modal onClose={toggleModal}>
-              <CarModal advert={car} />
-            </Modal>
-          )}
+    <li className={css.car_item}>
+      <div className={css.card}>
+        <div>
+          <img className={css.car_img} src={img} alt="car" />
         </div>
-      </li>
+        <button className={css.btn_favorite} type="button">
+          <svg className={css.favorite_svg} width="18" height="18">
+            <use href={heart + '#heart'}></use>
+          </svg>
+        </button>
+        <div className={css.info}>
+          <p>
+            {make}
+            {make.length <= 5 && (
+              <span className={css.model}> {model}</span>
+            )}, {year}
+          </p>
+          <p>{rentalPrice}</p>
+        </div>
+        <div className={css.about}>
+          <p>
+            <span>{city} | </span>
+            <span>{country} | </span>
+            <span>{rentalCompany} | </span>
+          </p>
+
+          <p>
+            <span>{type} | </span>
+            <span>{model} | </span>
+            <span>{id} | </span>
+          </p>
+        </div>
+        <Button text={'Learn more'} onClick={toggleModal} />
+        {showModal && (
+          <Modal onClose={toggleModal}>
+            <CarModal advert={car} />
+          </Modal>
+        )}
+      </div>
+    </li>
   );
 }
